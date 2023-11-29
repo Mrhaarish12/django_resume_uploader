a1b72ff9eb2581f95b8672b19a6d3706a9d51d90
Haarish Khan <khan1281979harry@gmail.com>
2023-11-30 01:40:27 +0530

    Forms and templates added

---
+++ myapp/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Resume
+
+class ResumeForm(forms.ModelForm):
+    class Meta:
+        model = Resume
+        fields = ['id','name','dob','gender','locality','city','pin','state','mobile','job_city','profile_image','myfile']
