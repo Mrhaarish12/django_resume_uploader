7c8681afa1e39c9527322de2bfcbebcfdb5fe1de
Haarish Khan <khan1281979harry@gmail.com>
2023-11-30 01:50:32 +0530

    forms modified

--- myapp/forms.py
+++ myapp/forms.py
@@ -5,3 +5,16 @@ class ResumeForm(forms.ModelForm):
     class Meta:
         model = Resume
         fields = ['id','name','dob','gender','locality','city','pin','state','mobile','job_city','profile_image','myfile']
+
+        labels = {'name':'Full Name', 'dob': 'Date of Birth', 'pin':'Pin Code', 'mobile':'Mobile No.', 'email':'Email ID', 'profile_image':'Profile Image', 'myfile':'Document'}
+
+        widgets = {
+            'name':forms.TextInput(attrs={'class':'form-control'}),
+            'dob':forms.DateInput(attrs={'class':'form-control', 'id':'datepicker'}),
+            'locality':forms.TextInput(attrs={'class':'form-control'}),
+            'city':forms.TextInput(attrs={'class':'form-control'}),
+            'pin':forms.NumberInput(attrs={'class':'form-control'}),
+            'state':forms.Select(attrs={'class':'form-select'}),
+            'mobile':forms.NumberInput(attrs={'class':'form-control'}),
+            'email':forms.EmailInput(attrs={'class':'form-control'}),
+        }
